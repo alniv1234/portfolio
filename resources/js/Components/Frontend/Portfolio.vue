31205bf5375bf21d17731b603be3df05f90f283d
Alniv <alnivcastillo@gmail.com>
2023-03-10 10:10:39 +0800

    display projects in page

--- resources/js/Components/Frontend/Portfolio.vue
+++ resources/js/Components/Frontend/Portfolio.vue
@@ -2,7 +2,7 @@
     <section id="section" class="bg-light-primary min-h-[1400px]">
         <div class="container mx-auto">
             <div class="flex flex-col items-center text-center">
-                <h2 >My Latest Work</h2>
+                <h2 class="text-3xl lg:text-4xl font-medium lg:font-extrabold m-10">My Latest Work</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim aspernatur adipisci nostrum. Amet quaerat veniam dicta vero distinctio dolor
                      blanditiis, voluptatum odio culpa, eveniet commodi quam molestiae iste fugiat dignissimos.</p>
             </div>
--- resources/js/Components/Frontend/Projects.vue
+++ resources/js/Components/Frontend/Projects.vue
@@ -3,23 +3,52 @@
         <nav class="mb-12 border-b-2 border-light-fourth ">
             <ul class="flex flex-col lg:flex-row justify-evenly items-center">
                 <li class=" cursor-pointer capitalize m-4">
-                    <button class=" text-center px-4 py-2 hover:text-light-fourth">
+                    <button @click="filterProject('all')" class=" text-center px-4 py-2 hover:text-light-fourth"
+                    :class="[
+                        selectedskill === 'all' ? 'text-light-fourth' : ''
+                    ]"
+                    >
                         All
                     </button>
                 </li>
                 <li class=" cursor-pointer capitalize m-4" v-for="projectSkill in skills.data" :key="projectSkill.id">
-                    <button class=" text-center px-4 py-2 hover:text-light-fourth" >
+                    <button @click="filterProject(projectSkill.id)" class=" text-center px-4 py-2 hover:text-light-fourth"
+                    :class="[
+                        selectedskill === projectSkill.id ? 'text-light-fourth' : ''
+                    ]"
+                    >
                         {{ projectSkill.name }}
                     </button>
                 </li>
             </ul>
         </nav>
+        <section class="grid gap-y-12 lg:grid-cols-3 lg:gap-8">
+            <Project v-for="project in filteredProjects" :key="project.id" :project="project" />
+        </section>
     </div>
 </template>
 
 <script setup>
-    defineProps({
+    import Project from './Project.vue';
+    import { ref } from "vue"
+
+    const props = defineProps({
         skills: Object,
         projects: Object
     })
+
+    const filteredProjects = ref(props.projects.data);
+    const selectedskill = ref("all")
+
+    const filterProject = (id) => {
+        if(id === "all"){
+            filteredProjects.value =props.projects.data
+            selectedskill.value = id
+        }else{
+            filteredProjects.value =props.projects.data.filter(project=> {
+                return project.skill.id === id
+            })
+            selectedskill.value = id
+        }
+    }
 </script>
